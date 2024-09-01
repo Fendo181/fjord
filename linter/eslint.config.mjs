b1f7026c742abfb9c15533e87797560851b42126
Fendo181 <fendo181git@gmail.com>
2024-09-01 14:51:49 +0900

    npm init @eslint/config@latest

--- linter/eslint.config.mjs
+++ linter/eslint.config.mjs
@@ -1,29 +1,11 @@
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
-export default [...compat.extends("eslint:recommended"), {
-    plugins: {},
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
-
-        ecmaVersion: 5,
-        sourceType: "commonjs",
-    },
-
-    rules: {},
-}];
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
